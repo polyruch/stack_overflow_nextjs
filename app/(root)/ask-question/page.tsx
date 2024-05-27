8c01d2eee8bb26f07855d223b28b56744900fa52
Mac <khpoly0@gmail.com>
2024-05-27 23:58:42 +0100

    added auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Page() {
+  return <div>ask a question</div>;
+}
+
+export default Page;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Home page(root)</div>;
+};
+
+export default page;
